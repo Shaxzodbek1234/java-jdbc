138e1b2ee46bb2ff5931c755c55c0275de628e1e
Shaxzodbek muqimov <shaxzodmuqimov2210@gmail.com>
2024-01-17 18:07:53 +0500

    Git commit

---
+++ src/main/java/uz/shaxzod/Main.java
@@ -0,0 +1,10 @@
+package uz.shaxzod;
+
+/**
+ * @author Shaxzod Muqimov, ${DAY_NAME_SHORT} ${TIME}. ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
